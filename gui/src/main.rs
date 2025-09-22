ba84b28b1a05cd2089eb689df07c69b8c9e22b56
Dom :) <dominic.j.milton@gmail.com>
2025-09-22 10:42:41 +0100

    example gui
    Signed-off-by: Dom :) <dominic.j.milton@gmail.com>

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,34 +1,33 @@
-fn mainmenu() {
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+#![allow(rustdoc::missing_crate_level_docs)] // it's an example
 
-}
+use eframe::egui;
 
-fn settings() {
+fn main() -> eframe::Result {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
 
-}
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        ..Default::default()
+    };
 
-fn modelinfo() {
+    // Our application state:
+    let mut name = "Arthur".to_owned();
+    let mut age = 42;
 
-}
-
-fn chat() {
-
-}
-
-fn themes(theme) {
-    if theme == "light" {
-
-    }
-
-    else if theme == "dark" {
-
-    }
-
-    else if theme == "greyscale" {
-
-    }
-    
-}
-
-fn main() {
-    println!("Hello, world!");
+    eframe::run_simple_native("My egui App", options, move |ctx, _frame| {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("My egui Application");
+            ui.horizontal(|ui| {
+                let name_label = ui.label("Your name: ");
+                ui.text_edit_singleline(&mut name)
+                    .labelled_by(name_label.id);
+            });
+            ui.add(egui::Slider::new(&mut age, 0..=120).text("age"));
+            if ui.button("Increment").clicked() {
+                age += 1;
+            }
+            ui.label(format!("Hello '{name}', age {age}"));
+        });
+    })
 }
