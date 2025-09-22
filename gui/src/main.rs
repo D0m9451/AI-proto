3aa8b2c74a2c01464d48e3e567ca3f661445e97b
Dom :) <dominic.j.milton@gmail.com>
2025-09-22 09:48:46 +0100

    gui start
    Signed-off-by: Dom :) <dominic.j.milton@gmail.com>

---
+++ gui/src/main.rs
@@ -0,0 +1,34 @@
+fn mainmenu() {
+
+}
+
+fn settings() {
+
+}
+
+fn modelinfo() {
+
+}
+
+fn chat() {
+
+}
+
+fn themes(theme) {
+    if theme == "light" {
+
+    }
+
+    else if theme == "dark" {
+
+    }
+
+    else if theme == "greyscale" {
+
+    }
+    
+}
+
+fn main() {
+    println!("Hello, world!");
+}
