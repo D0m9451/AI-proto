27b7b58cd668f5e503163b329179d618a913dcab
Dom :) <dominic.j.milton@gmail.com>
2026-02-06 00:43:08 +0000

    Good structure... structure eccellent
    Signed-off-by: Dom :) <dominic.j.milton@gmail.com>

--- memory/src/main.rs
+++ memory/src/main.rs
@@ -1,15 +1,59 @@
 use std::sync::{Arc, Mutex};
 use std::net::{TcpListener, TcpStream};
-use std::io::Read;
+use std::io::{Read, Write};
 use std::thread;
 
 struct ShortMem {
-    last_user: Option<String>,
-    last_assistant: Option<String>,
+    user: Option<String>,
+    model: Option<String>,
+}
+
+fn construct(mem: &ShortMem) -> String {
+    let user = mem.user.as_deref().unwrap_or("");
+    let model = mem.model.as_deref().unwrap_or("");
+
+    format!("Context:\n{}\n{}", user, model)
 }
 
 
+fn guilisten<T>() {
+    let listener = TcpListener::bind("127.0.0:1:9090").unwrap();
+    for stream in listener.incoming() {
+        let user = Arc::new(Mutex::new(None::<T>));
+    }
+}
+
+fn modellisten<T>() {
+    let listener = TcpListener::bind("127.0.0:1:8080").unwrap();
+    for stream in listener.incoming() {
+        let model = Arc::new(Mutex::new(None::<T>));
+    }
+}
+
+
+
+fn sendgui() {
+    let mut stream = TcpStream::connect("127.0.0:1:9090").unwrap();
+    stream.write_all(b"RAHGOOO").unwrap();
+}
+
+fn sendmodel() {
+    let mut stream = TcpStream::connect("127.0.0:1:8080").unwrap();
+    stream.write_all(b"GOOORAH").unwrap();
+}
+
+
+
+
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:9090").unwrap();
-    
+    //guilisten();
+
+    let mem = ShortMem {
+        user: Some("Hello".to_string()),
+        model: Some("Hi!".to_string()),
+    };
+
+
+    let prompt = construct(&mem);
+    println!("{}", prompt); 
 }
